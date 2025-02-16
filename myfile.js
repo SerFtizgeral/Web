ab985138930465d1bd52cd5a03bb9583f5fdca1e
SirFtizgeral <francisjeevesjulayco@gmail.com>
2025-02-16 17:28:22 +0800

    Enhanced the page 1

--- myfile.js
+++ myfile.js
@@ -1,3 +1,3 @@
 function onClickEvent(){
-    window.location.href = "https://www.google.com/";
+    window.location.href = "https://serftizgeral.github.io/Franz/?fbclid=IwY2xjawF4c69leHRuA2FlbQIxMAABHej4QBTFSs5_FbmbgH9b7eOx9wt7-LH1UDG18Yz9zzd1Y2Md9VfYydef5w_aem_Beye9yg99KvWMb9rPiNsPw";
 }
